949668f3baccb8ad4fd7ec74fa39d314296a2749
Michael Rapadas <mike@mrap.me>
2014-04-21 01:25:45 -0700

    Connection origin and target must be the same type

--- model/connection.js
+++ model/connection.js
@@ -1,5 +1,9 @@
+var mongoose = require('mongoose');
 
 var Connection = function Connection(origin, target, distance){
+  if (origin.constructor.name != target.constructor.name)
+    throw new Error("connection origin and target must be of the same type");
+
   this.origin = origin;
   this.target = target;
   this.distance = distance;
