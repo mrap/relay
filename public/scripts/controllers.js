f5f00f04922f8bcea8bd1e6d916f1e14b26883cd
Michael Rapadas <mike@mrap.me>
2014-05-03 15:16:39 -0700

    Moves currentUser logic into userManager.
    Uses:
      + $rootScope.currentUser
      + $rootScope.isUserLoggedIn

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -3,11 +3,7 @@
 var controllers = angular.module('relay.controllers', []);
 
 controllers.controller('AppController', ['$scope', 'userManager', function($scope, userManager){
-  $scope.isUserLoggedIn = false;
-  $scope.$watch('currentUser', function(){
-    $scope.isUserLoggedIn = !_.isEmpty($scope.currentUser);
-  }, true);
-  $scope.currentUser = userManager.getCurrentUser();
+  userManager.setCurrentUser();
 }]);
 
 controllers.controller('PostsController', ['$scope', function(scope){
--- public/scripts/factories.js
+++ public/scripts/factories.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var factories = angular.module('relay.factories', [
   'restangular'
 ]);
@@ -10,11 +12,45 @@ factories.factory('feedManager', ['Restangular', function(Restangular){
   };
 }]);
 
-factories.factory('userManager', ['Restangular', function(Restangular){
+factories.factory('userManager', ['$rootScope', 'Restangular', 'GuestUser', function($rootScope, Restangular, GuestUser){
 
   return {
-    getCurrentUser: function(){
-      return Restangular.one('loggedIn').get().$object;
+    setCurrentUser: function(){
+      $rootScope.isUserLoggedIn = false;
+      $rootScope.currentUser = new GuestUser();
+      Restangular.one('loggedIn').get().then(function(user){
+        $rootScope.currentUser    = user;
+        $rootScope.isUserLoggedIn = true;
+      });
     }
   };
+
+}]);
+
+factories.factory('User', function(){
+
+  function User(data){
+    angular.extend(this, {
+      // Instance defaults
+    });
+    angular.extend(this, data);
+  }
+
+  return User;
+});
+
+factories.factory('GuestUser', ['User', function(User){
+
+  function GuestUser(data) {
+    angular.extend(this, {
+      deferredRelays:      {},
+      deferRelay:          function(post) { this.deferredRelays[post.id] = post },
+      removeDeferredRelay: function(post) { delete this.deferredRelays[post.id] }
+    });
+
+    // Inherit from User
+    angular.extend(this, User);
+  }
+
+  return GuestUser;
 }]);
