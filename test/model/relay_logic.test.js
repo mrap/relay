53def3bb05ef99427228f2cf225f82e9e9a04e6b
Michael Rapadas <mike@mrap.me>
2014-05-04 11:02:12 -0700

    Adds a pending test for user#unrelayPost

--- test/model/relay_logic.test.js
+++ test/model/relay_logic.test.js
@@ -35,6 +35,7 @@ describe("Relay Logic", function(){
     });
   });
 
+  /*** Relaying a post ***/
   describe("user relays a connected user's post", function(){
     beforeEach(function(done){
       user.relayOtherPost(post, function(err, res){
@@ -69,6 +70,49 @@ describe("Relay Logic", function(){
         if (err) return done(err);
         expect(feedItem.relayed).to.be.true;
         done();
+      });
+    });
+
+
+    /*** Un-relaying a post ***/
+    // TODO: Reverse a relay.
+    // Reverse all changes as they were before relay.
+    // This might require a transaction stack of some sort.
+    describe.skip("user unrelays a post", function(){
+      beforeEach(function(done){
+        user.unrelayPost(post, function(err, res){
+          if (err) done(err);
+          done()
+        });
+      });
+
+      it("should reset distance to author", function(done){
+        user.getDistanceToUser(author, function(err, dist){
+          dist.should.not.eq(-1);
+          dist.should.be.eq(initConnDist);
+          done();
+        });
+      });
+
+      it("should make all other connections closer to user (back to original distance)", function(done){
+        function checkAnother(current){
+          if (current >= connectionsCount) return done();
+          var cUser = connUsers[current];
+          if (cUser._id.toString() === author._id.toString()) return checkAnother(current+1);
+          user.getDistanceToUser(cUser, function(err, dist){
+            dist.should.be.eq(initConnDist);
+            return checkAnother(current+1);
+          });
+        }
+        checkAnother(0);
+      });
+
+      it("should mark feedItem's relayed attribute to false", function(done){
+        FeedManager.getUserFeedItem(user, post, function(err, feedItem){
+          if (err) return done(err);
+          expect(feedItem.relayed).to.be.false;
+          done();
+        });
       });
     });
   });
