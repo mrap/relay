b5dad44b6c2526f87c493b49df11e57befae7616
Michael Rapadas <mike@mrap.me>
2014-05-04 22:31:50 -0700

    Refactors feed directive -> HomeFeedController

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -6,5 +6,16 @@ controllers.controller('AppController', ['$scope', 'userManager', function($scop
   userManager.setCurrentUser();
 }]);
 
-controllers.controller('FeedController', ['$scope', function(scope){
+controllers.controller('HomeFeedController', ['$rootScope', '$scope', 'feedManager', function($rootScope, $scope, feedManager){
+
+    $rootScope.$watch('isUserLoggedIn', function(){
+      if (!$rootScope.isUserLoggedIn) $scope.posts = feedManager.getPopularPosts();
+      else                            $scope.posts = $rootScope.currentUser.getFeed();
+    }, true);
+
+    $scope.toggleRelayPost = function(post) {
+      if (post.relayed) $rootScope.currentUser.unrelayPost(post);
+      else              $rootScope.currentUser.relayPost(post);
+    };
+
 }]);
--- public/scripts/directives.js
+++ public/scripts/directives.js
@@ -1,24 +1,3 @@
 'use strict';
 
 var directives = angular.module('relay.directives', []);
-
-directives.directive('feed', ['$rootScope', 'feedManager', function($rootScope, feedManager){
-  function link(scope, element, attrs){
-
-    scope.$watch('posts', function(){
-      // Do stuff when 'posts' data is updated
-    }, true);
-
-    if (attrs.publicFeed) {
-      if(attrs.publicFeed === 'popular'){
-        scope.posts = feedManager.getPopularPosts();
-      }
-    }
-
-    scope.toggleRelayPost = function(post) {
-      if (post.relayed) $rootScope.currentUser.unrelayPost(post);
-      else              $rootScope.currentUser.relayPost(post);
-    };
-  }
-  return { link: link }
-}]);
